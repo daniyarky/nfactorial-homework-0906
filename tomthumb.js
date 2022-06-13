cf0e3a90c9a1276708040ac2ff7ecb4c9a038a42
Данияр Куанышбаев <danik.kuanyshbayev@gmail.com>
2022-06-13 13:43:58 +0600

    hw09

--- tomthumb.js
+++ tomthumb.js
@@ -7,14 +7,39 @@ $('document').ready(function(){
         // $('result').text($(this).val')()+$(this).find(':selected').data('price'))
     });
     $('button').click(function(){
-        var x= $('#fname').val();
-        var y=$('#cname').val();
-        var z=$('#adverb').val();
-        var v=$('#adjective').val();
+    
+        $('h2').empty();
+        // $('h2').append(getBitDepth());
+        $('h2').append('File size is '+indexedFileSize()+' kB');
+        if(indexedFileSize()<=50){
+            $('h2').append(' which is ok');
+        }   else if(indexedFileSize()>50){
+            $('h2').append(' which is not ok');
+        }   else{
+            $('h2').empty();
+            $('h2').append('smth went wrong, make sure you entered correct number values');
 
+        }   
 
-        $('h2').append();
-        
     });
 
+    
+    
+
+    // $('h2').append( getBitDepth($('#Colors').val()));
+
+
+
+
 });
+
+function getBitDepth() {
+    var x=Math.log($('#nc').val()) / Math.log(2);
+    return x;
+}
+
+function indexedFileSize() {
+    var y=($('#h').val()*$('#w').val()*(Math.log($('#nc').val()) / Math.log(2)))/8/1000;
+    return y;
+      
+}
